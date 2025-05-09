f42f835b2996e405eb7b4da372731c5cbcc79610
ദ്ദി˶𖥦)🍋 <rhyme.qing@gmail.com>
2025-05-09 22:06:24 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
-import LogoIcon from '@site/src/components/HomepageFeatures/Logo';
+import LogoIcon from '@site/src/components/HomepageFeatures/logo';
 
 import styles from "./index.module.css";
 
