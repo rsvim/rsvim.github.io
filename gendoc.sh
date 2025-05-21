e8ac4408de402dc31eb226ac2f2b5e97c62d9363
linrongbin16 <linrongbin16@outlook.com>
2025-05-21 16:28:48 +0800

    chore

--- gendoc.sh
+++ gendoc.sh
@@ -4,32 +4,55 @@
 # 1. `npm install -g typedoc`
 # 2. `npm install`
 
-echo "(rsvim.github.io) PWD=${PWD}"
-REPOSITORY="${PWD}/../rsvim"
-echo "(rsvim) REPOSITORY=${REPOSITORY}"
+CUR_DIR="${PWD}"
+RSVIM_DIR="${PWD}/../rsvim"
+RSVIM_JSRUNTIME_SRC_DIR="$RSVIM_DIR/rsvim_core/src/js/runtime"
+API_DOCS_DST_DIR="${PWD}/docs/api"
+TMP_JSRUNTIME_DIR="${PWD}/.jsruntime"
+TMP_GENERATED_DIR="${PWD}/.generated"
 
-JSRUNTIME="${PWD}/jsruntime"
-GENERATED_TYPEDOCS="${PWD}/generated-typedocs"
-echo "copy typescripts to $JSRUNTIME"
-rm -rf ./jsruntime
-rm -rf ./generated-typedocs
-cp -rf $REPOSITORY/rsvim_core/src/js/runtime $JSRUNTIME
+echo "CWD:$CUR_DIR"
+echo "RSVIM dir:$RSVIM_DIR"
+echo "RSVIM jsruntime source dir:$RSVIM_JSRUNTIME_SRC_DIR"
+echo "TMP jsruntime dir:$TMP_JSRUNTIME_DIR"
+echo "TMP generated dir:$TMP_GENERATED_DIR"
+
+if [ ! -d "$RSVIM_DIR" ]; then
+  echo "Error! RSVIM dir ($RSVIM_DIR) not exist, exit..."
+  exit 1
+fi
+if [ ! -d "$RSVIM_JSRUNTIME_SRC_DIR" ]; then
+  echo "Error! RSVIM jsruntime source dir ($RSVIM_JSRUNTIME_SRC_DIR) not exist, exit..."
+  exit 1
+fi
+
+# cleanup old
+rm -rf $TMP_JSRUNTIME_DIR
+rm -rf $TMP_GENERATED_DIR
+
+echo "copy RSVIM jsruntime source ($RSVIM_JSRUNTIME_SRC_DIR) to $TMP_JSRUNTIME_DIR"
+cp -rf $RSVIM_JSRUNTIME_SRC_DIR $TMP_JSRUNTIME_DIR
 
 echo "npm run typedoc"
 npm run typedoc
 
-API="${PWD}/docs/api"
-echo "copy generated docs to ${API}"
+copy_api_doc() {
+  local name="$1"
+  local api_doc="$API_DOCS_DST_DIR/$name"
+  local gen_doc="$TMP_GENERATED_DIR/$name"
+  echo "copy generated docs ($gen_doc) to $API_DOCS_DST_DIR"
+  rm -rf $api_doc
+  cp -rf $gen_doc $API_DOCS_DST_DIR
+}
 
-_00_WEB=00__web
-_01_RSVIM=01__rsvim
+copy_api_doc "00__web"
+copy_api_doc "01__rsvim"
 
-rm -rf ${API}/${_00_WEB}
-rm -rf ${API}/${_01_RSVIM}
-cp -rf ${GENERATED_TYPEDOCS}/${_00_WEB} ${API}
-cp -rf ${GENERATED_TYPEDOCS}/${_01_RSVIM} ${API}
+# cleanup
+rm -rf $TMP_JSRUNTIME_DIR
+rm -rf $TMP_GENERATED_DIR
 
 echo "git status"
 git status
-echo "tree ${API}"
-tree ${API}
+echo "tree $API_DOCS_DST_DIR"
+tree $API_DOCS_DST_DIR
