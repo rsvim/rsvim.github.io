a4b1bd32216878da1ac3ddcf8242075e2edcf805
linrongbin16 <linrongbin16@outlook.com>
2025-10-29 09:48:57 +0800

    c

--- gendoc.py
+++ gendoc.py
@@ -12,15 +12,18 @@ from pathlib import Path
 
 CWD = Path(os.getcwd())
 RSVIM_SRC = CWD.parent / "rsvim" / "runtime"
-RSVIM_SRC_FILES = [RSVIM_SRC / filename for filename in ["00__web.ts", "01__rsvim.ts"]]
+RSVIM_SRC_FILENAMES = ["00__web.ts", "01__rsvim.ts"]
+RSVIM_SRC_FILES = [RSVIM_SRC / filename for filename in RSVIM_SRC_FILENAMES]
 GHPAGE_SRC = CWD / "runtime"
+GHPAGE_SRC_FILES = [GHPAGE_SRC / filename for filename in RSVIM_SRC_FILENAMES]
 GHPAGE_DOC = CWD / "typedoc"
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     logging.info(f"CWD:{CWD}")
-    logging.info(f"RSVIM_SRC_DIR:{RSVIM_SRC}")
-    logging.info(f"RSVIM_SRC:{RSVIM_SRC_FILES}")
+    logging.info(f"RSVIM_SRC:{RSVIM_SRC}")
+    logging.info(f"RSVIM_SRC_FILES:{RSVIM_SRC_FILES}")
     logging.info(f"GHPAGE_SRC:{GHPAGE_SRC}")
+    logging.info(f"GHPAGE_SRC_FILES:{GHPAGE_SRC_FILES}")
     logging.info(f"GHPAGE_DOC:{GHPAGE_DOC}")
