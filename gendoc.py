b36c1dc4116cfb23a354edfafc97316e1e636660
linrongbin16 <linrongbin16@outlook.com>
2025-10-29 09:47:40 +0800

    c

--- gendoc.py
+++ gendoc.py
@@ -11,12 +11,16 @@ import shutil
 from pathlib import Path
 
 CWD = Path(os.getcwd())
-RSVIM_SRC_DIR = CWD.parent / "rsvim" / "runtime"
-RSVIM_SRC = [RSVIM_SRC_DIR / filename for filename in ["00__web.ts", "01__rsvim.ts"]]
+RSVIM_SRC = CWD.parent / "rsvim" / "runtime"
+RSVIM_SRC_FILES = [RSVIM_SRC / filename for filename in ["00__web.ts", "01__rsvim.ts"]]
+GHPAGE_SRC = CWD / "runtime"
+GHPAGE_DOC = CWD / "typedoc"
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
     logging.info(f"CWD:{CWD}")
-    logging.info(f"RSVIM_SRC_DIR:{RSVIM_SRC_DIR}")
-    logging.info(f"RSVIM_SRC:{RSVIM_SRC}")
+    logging.info(f"RSVIM_SRC_DIR:{RSVIM_SRC}")
+    logging.info(f"RSVIM_SRC:{RSVIM_SRC_FILES}")
+    logging.info(f"GHPAGE_SRC:{GHPAGE_SRC}")
+    logging.info(f"GHPAGE_DOC:{GHPAGE_DOC}")
